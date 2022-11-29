a4525f7f11351da3d668797c24f68f560397683e
Bertmoss <b3rtm0ss@gmail.com>
2022-11-29 12:18:04 +0100

    Style header

--- src/header/header.js
+++ src/header/header.js
@@ -1,12 +1,14 @@
 //Hamburger Menu Btn/* 
 let btn = document.querySelector(".header-div__btn");
 let nav = document.querySelector(".header__nav");
+let header = document.querySelector(":root");
 
 
 
 btn.addEventListener("click", () => {
-  btn.classList.toggle("open")
-  nav.classList.toggle("hidden")
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  header.classList.toggle("dark");
 }) 
 
 
