5b7d020bfded7d236ecf27652391c1f507451a53
Bertmoss <b3rtm0ss@gmail.com>
2022-12-01 17:21:17 +0100

    decoration

--- src/header/header.js
+++ src/header/header.js
@@ -1,14 +1,14 @@
 //Hamburger Menu Btn/* 
 let btn = document.querySelector(".header-div__btn");
 let nav = document.querySelector(".header__nav");
-let header = document.querySelector(":root");
+let root = document.querySelector(":root");
 
 
 
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   nav.classList.toggle("hidden");
-  header.classList.toggle("dark");
+  root.classList.toggle("dark");
 }) 
 
 
